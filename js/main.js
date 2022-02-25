d19d00bf6a371c8d4158714cd66e99bc485b746e
hna1707 <hna1707@gmail.com>
2022-02-25 16:16:06 +0900

    update

--- js/main.js
+++ js/main.js
@@ -30,6 +30,47 @@ $(function(){
         $('html, body').animate({scrollTop:scrollTotal},1000);
     }
 
-    
+        // 이미지 슬라이더
+        $('.slide-list').slick({
+            fade:true,
+            slidesToShow: 1,
+            draggable:false,
+            lazyLoad: 'progressive',
+            //autoplay:true,
+            autoplay: true,
+            autoplaySpeed:4000,
+            pauseOnFocus:false,
+            pauseOnHover:false,
+            arrows:false,            
+            dots:false, 
+            centerMode: true
+        });
 
+
+        let mouseCursor = document.querySelector(".cursor");
+        let Links = document.querySelectorAll("a"); //메뉴 링크
+        //window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
+        window.addEventListener("scroll", cursor);
+        window.addEventListener("mousemove", cursor);
+        //커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
+        function cursor(e) {
+          mouseCursor.style.left = e.pageX + "px";
+          mouseCursor.style.top = e.pageY - scrollY + "px";
+      }
+      Links.forEach((link) => {
+        link.addEventListener("mouseover", () => {
+          mouseCursor.classList.add("link-grow");
+          mouseCursor.style.zIndex = "-1";
+          link.classList.add("hovered-link");
+        });
+        link.addEventListener("mouseleave", () => {
+          mouseCursor.classList.remove("link-grow");
+          mouseCursor.style.zIndex = "1000";
+          link.classList.remove("hovered-link");
+        });
+      });
+
+        new WOW().init();
+
+        
 });
